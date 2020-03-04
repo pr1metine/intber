7eb2b0431b170a10c774c7ffde4fb3354abd696a
pr1metine <derthanh001@gmail.com>
2020-03-04 19:45:58 +0000

    Odertrenner ist kaputt

--- neuerLoginbereich.php
+++ neuerLoginbereich.php
@@ -4,6 +4,7 @@
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>INTBER - Login</title>
+	<link rel="stylesheet" href="css/login.css">
 	<link rel="stylesheet" href="css/neu-style.css">
 </head>
 
