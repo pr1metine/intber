fe5b5464c1d9480c2401305c0f4a64276fccb7ba
Quang Thanh Ta <derthanh001@gmail.com>
2020-03-05 18:18:59 +0100

    Unnötige Comps gelöscht

--- components/iserv-login-button.php
+++
@@ -1,4 +0,0 @@
-<button class="btn-iserv-oath" disabled>
-    <img src="img/IServ-Logo mit Box.svg" alt="IServ-Logo">
-    <span class="btn-text">Feature kommt irgendwann</span>
-</button>
--- components/oder-divider.php
+++
@@ -1,5 +0,0 @@
-<div class="seperator">
-    <hr>
-    <span class="sep-text">ODER</span>
-    <hr>
-</div>
